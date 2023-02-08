4da2402fd1f6212947a7c2e680eef81ca5f377f1
Dmitriy <vip.dimarus2@mail.ru>
2023-02-08 21:33:19 +0300

    Init commit

---
+++ src/main/java/ru/lds/telegrambotarb/TelegramBotArbApplication.java
@@ -0,0 +1,13 @@
+package ru.lds.telegrambotarb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotArbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotArbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/lds/telegrambotarb/TelegramBotArbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lds.telegrambotarb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotArbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
